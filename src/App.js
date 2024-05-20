29a8a48250bc004824ff30777935d33b4f751af4
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-21 01:13:50 +0300

    stop in Edit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,108 @@
 // import 'antd/dist/rest.css';
 import { useEffect, useState } from 'react';
 import './App.css';
-// import { Table, Tag } from 'antd';
-import Icon, {
-  AppleFilled,
-  BankTwoTone,
-  LoadingOutlined,
-  PieChartFilled,
-} from '@ant-design/icons';
-import { Divider } from 'antd';
+import { Table, Button, Modal } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 function App() {
-  const HardtIcon = (props) => {
-    return (
-      <Icon
-        component={() => {
-          return <svg></svg>;
-        }}
-        {...props}
-      />
-    );
+  const [dataSource, setDataSource] = useState([
+    {
+      id: 1,
+      name: 'John',
+      email: 'Johan@itcodx.com',
+      address: 'Johan Address',
+    },
+    {
+      id: 2,
+      name: 'Adam',
+      email: 'Adam@google.com',
+      address: 'Adam Address',
+    },
+    {
+      id: 3,
+      name: 'Jolia',
+      email: 'Jolia@Microsoft.com',
+      address: 'Jolia Address',
+    },
+    {
+      id: 4,
+      name: 'Kate',
+      email: 'kate@yahoo.com',
+      address: 'kate Address',
+    },
+  ]);
+  const columns = [
+    {
+      key: '1',
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      key: '2',
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      key: '3',
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      key: '4',
+      title: 'Address',
+      dataIndex: 'address',
+    },
+    {
+      key: '5',
+      title: 'Actions',
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined
+              style={{ fontSize: '18px', color: 'blue', margin: '0 8px' }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                onDeleteStudent(record);
+              }}
+              style={{ fontSize: '18px', color: 'red', margin: '0 8px' }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+  const onAddStudent = () => {
+    const randomNumber = parseInt(Math.random() * 1e9);
+    const newStudent = [
+      {
+        id: randomNumber,
+        name: 'Name ' + randomNumber,
+        email: randomNumber + '@google.com',
+        address: 'Address ' + randomNumber,
+      },
+    ];
+    setDataSource((pre) => {
+      return [...pre, newStudent];
+    });
+  };
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: 'Are you want to Delete This Student',
+      okText: 'Yes',
+      okType: 'danger',
+      onOK: () => {
+        setDataSource((pre) => {
+          return pre.filter((student) => student.id !== record.id);
+        });
+      },
+    });
   };
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <PieChartFilled style={{ color: 'purple', fontSize: '62px' }} />
-          <AppleFilled style={{ color: 'green', fontSize: '62px' }} />
-          <LoadingOutlined
-            rotate={45}
-            style={{ color: 'red', fontSize: '62px' }}
-          />
-          <BankTwoTone twoToneColor="green" style={{ fontSize: '62px' }} />
-          <Divider />
-        </div>
+        <Button onClick={onAddStudent}>Add a new Studet</Button>
+        <Table dataSource={dataSource} columns={columns}></Table>
       </header>
     </div>
   );
