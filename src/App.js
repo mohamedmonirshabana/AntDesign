aff4ed80d5f1878ed10dbff529cb389fb995bdb6
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 15:07:29 +0300

    add my

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import './App.css';
 import { Input } from 'antd';
-import { PoweroffOutlined } from '@ant-design/icons';
+import { UserOutlined, EyeFilled, EyeInvisibleFilled } from '@ant-design/icons';
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -16,7 +16,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Input placeholder="Name" maxLength={10} type="password"></Input>
+        <Input
+          placeholder="Name"
+          maxLength={10}
+          prefix={<UserOutlined />}
+        ></Input>
+        <Input
+          placeholder="password"
+          type="password"
+          prefix={<EyeFilled />}
+        ></Input>
       </header>
     </div>
   );
