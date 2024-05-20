dba9ac002ccc838ccd44b39541bac00b7438cd61
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 16:15:03 +0300

    add Alert

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
 // import 'antd/dist/rest.css';
 import { useState } from 'react';
 import './App.css';
-import { Table } from 'antd';
+import { Form, Input, Button, message, Alert } from 'antd';
 
 function App() {
-  const data = [
-    { name: 'Name1', age: 20, address: 'Address 1', kay: '1' },
-    { name: 'Name2', age: 21, address: 'Address 2', kay: '2' },
-    { name: 'Name3', age: 22, address: 'Address 3', kay: '3' },
-    { name: 'Name4', age: 23, address: 'Address 4', kay: '4' },
-    { name: 'Name5', age: 24, address: 'Address 5', kay: '5' },
-    { name: 'Name6', age: 25, address: 'Address 6', kay: '6' },
-  ];
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'key',
-      render: (name) => {
-        return <a>{name}</a>;
-      },
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'key',
-      sorter: (a, b) => a.age - b.age,
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'key',
-    },
-    {
-      title: 'Granduated',
-      key: 'key',
-      render: (payload) => {
-        return <p>{payload.age > 20 ? 'True' : 'false'}</p>;
-      },
-    },
-  ];
-
+  const [showAlert, setshowAlert] = useState(false);
+  const onFinish = (e) => {
+    console.log(e);
+    setTimeout(() => {
+      setshowAlert(true);
+      // message.success('Log in sucess');
+      // message.error('Login failed');
+      // message.warning('Login failed');
+    }, 2000);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Table dataSource={data} columns={columns}></Table>
+        {showAlert && (
+          <Alert
+            type="error"
+            message="Error"
+            description="There is error in password"
+            closable
+          />
+        )}
+        <Form onFinish={onFinish}>
+          <Form.Item label="user Name" name="username">
+            <Input placeholder="UserName" required></Input>
+          </Form.Item>
+          <Form.Item label="Password" name="passwword">
+            <Input.Password placeholder="Password" required></Input.Password>
+          </Form.Item>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
