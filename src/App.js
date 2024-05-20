b08733afa419f77927ee3290d801e5db39852b6b
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 15:30:56 +0300

    Login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 // import 'antd/dist/rest.css';
 import { useState } from 'react';
 import './App.css';
-import { Select } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 function App() {
-  const fruits = ['Banana', 'mango', 'orange', 'cherry'];
+  const onFinish = (e) => {
+    console.log(e);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <p>Which is your Favorite fruit?</p>
-        <Select
-          mode="multiple"
-          maxTagCount={2}
-          allowClear
-          placeholder="Select Fruit"
-          style={{ width: '50%' }}
-        >
-          {fruits.map((fruit, index) => {
-            return (
-              <Select.Option key={index} value={index}>
-                {fruit}
-              </Select.Option>
-            );
-          })}
-        </Select>
+        <Form onFinish={onFinish}>
+          <Form.Item label="user Name" name="username">
+            <Input placeholder="user name" required></Input>
+          </Form.Item>
+          <Form.Item label="password" name="userpassword">
+            <Input.Password placeholder="user userpassword"></Input.Password>
+          </Form.Item>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
