8c52c812a25382bccf548b20c6276754908196e9
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 15:54:14 +0300

    table

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 // import 'antd/dist/rest.css';
 import { useState } from 'react';
 import './App.css';
-import { Form, Input, Button } from 'antd';
+import { Table } from 'antd';
 
 function App() {
-  const onFinish = (e) => {
-    console.log(e);
-  };
+  const data = [
+    { name: 'Name1', age: 20, address: 'Address 1', kay: '1' },
+    { name: 'Name2', age: 21, address: 'Address 2', kay: '2' },
+    { name: 'Name3', age: 22, address: 'Address 3', kay: '3' },
+    { name: 'Name4', age: 23, address: 'Address 4', kay: '4' },
+    { name: 'Name5', age: 24, address: 'Address 5', kay: '5' },
+    { name: 'Name6', age: 25, address: 'Address 6', kay: '6' },
+  ];
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'key',
+      render: (name) => {
+        return <a>{name}</a>;
+      },
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'key',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'key',
+    },
+    {
+      title: 'Granduated',
+      key: 'key',
+      render: (payload) => {
+        return <p>{payload.age > 20 ? 'True' : 'false'}</p>;
+      },
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form onFinish={onFinish}>
-          <Form.Item label="user Name" name="username">
-            <Input placeholder="user name" required></Input>
-          </Form.Item>
-          <Form.Item label="password" name="userpassword">
-            <Input.Password placeholder="user userpassword"></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button block type="primary" htmlType="submit">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+        <Table dataSource={data} columns={columns}></Table>
       </header>
     </div>
   );
