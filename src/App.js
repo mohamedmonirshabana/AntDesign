7f196cf03b37f8dd5456a670d64d402a5b813446
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 20:25:31 +0300

    show Table Data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 // import 'antd/dist/rest.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Progress } from 'antd';
+import { Table } from 'antd';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [datSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/todos', { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        setDataSource(data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  const columns = [
+    {
+      key: '1',
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      key: '2',
+      title: 'user Id',
+      dataIndex: 'userId',
+    },
+    {
+      key: '3',
+      title: 'Status',
+      dataIndex: 'completed',
+      render: (completed) => {
+        return <p>{completed ? 'complete' : 'In progress'}</p>;
+      },
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <Progress percent={33} />
-        <Progress percent={33} type="circle" />
-        <Progress percent={33} type="line" strokeColor="red" />
-        <Progress percent={33} type="line" strokeColor="red" strokeWidth={50} />
-        <Progress percent={33} type="line" strokeColor="red" status="active" />
-        <Progress percent={33} type="circle" status="success" />
-        <Progress percent={33} type="circle" status="exception" />
-        <Progress percent={33} type="line" status="success" />
-        <Progress percent={33} type="line" status="success" steps={3} />
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={datSource}
+        ></Table>
       </header>
     </div>
   );
