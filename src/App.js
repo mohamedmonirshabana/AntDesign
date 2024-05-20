ef52ed6cd0cec9714d5027c308b546c40aa687b7
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 19:39:02 +0300

    add spin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import 'antd/dist/rest.css';
 import { useState } from 'react';
 import './App.css';
-import { DatePicker, TimePicker } from 'antd';
+import { Button, Spin } from 'antd';
 
 function App() {
+  const [spinPlay, setSpinPlay] = useState(true);
+  const changeSpin = () => {
+    setSpinPlay((preValue) => !preValue);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <DatePicker picker="quarter" disabled />
-        <DatePicker.RangePicker picker="month" />
-        <TimePicker />
+        <Spin spinning={spinPlay}></Spin>
+        <Button onClick={changeSpin}>Toggle Button</Button>
+        <Spin spinning={spinPlay}>
+          <p>P tag 1</p>
+          <p>P tag 2</p>
+          <p>P tag 3</p>
+        </Spin>
       </header>
     </div>
   );
