9593bb7e59e9b140d8ce982b2f5aed9c4019ef28
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-20 16:23:04 +0300

    datePicker

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 // import 'antd/dist/rest.css';
 import { useState } from 'react';
 import './App.css';
-import { Form, Input, Button, message, Alert } from 'antd';
+import { DatePicker, TimePicker } from 'antd';
 
 function App() {
-  const [showAlert, setshowAlert] = useState(false);
-  const onFinish = (e) => {
-    console.log(e);
-    setTimeout(() => {
-      setshowAlert(true);
-      // message.success('Log in sucess');
-      // message.error('Login failed');
-      // message.warning('Login failed');
-    }, 2000);
-  };
   return (
     <div className="App">
       <header className="App-header">
-        {showAlert && (
-          <Alert
-            type="error"
-            message="Error"
-            description="There is error in password"
-            closable
-          />
-        )}
-        <Form onFinish={onFinish}>
-          <Form.Item label="user Name" name="username">
-            <Input placeholder="UserName" required></Input>
-          </Form.Item>
-          <Form.Item label="Password" name="passwword">
-            <Input.Password placeholder="Password" required></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button block type="primary" htmlType="submit">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+        <DatePicker picker="quarter" disabled />
+        <DatePicker.RangePicker picker="month" />
+        <TimePicker />
       </header>
     </div>
   );
